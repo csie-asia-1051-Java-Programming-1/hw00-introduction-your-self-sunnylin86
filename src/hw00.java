d92a9f725937f7288caaca63aeb31fb6afd0b070
sunnylin86 <105021101@live.asia.edu.tw>
2017-02-24 14:58:57 +0800

    hw submit

---
+++ src/hw00.java
@@ -0,0 +1,15 @@
+/*
+* Topic: 自我介紹
+* Date: 2017/02/24
+* Author: 105021101 林昱成
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("學號:105021101");
+	System.out.println("姓名:林昱成");
+	System.out.println("資工系資電組1A");
+	}
+
+}
